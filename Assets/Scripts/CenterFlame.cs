1cc4058f3564fef5bdaa858ac1a160a2c7eb20f6
hwannow <vnfdlvgid061@naver.com>
2022-07-06 13:28:48 +0900

    add eee

--- Assets/Scripts/CenterFlame.cs
+++ Assets/Scripts/CenterFlame.cs
@@ -16,7 +16,7 @@ public class CenterFlame : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(!musicStart){
-			Debug.Log("very hungry");
+			Debug.Log("veeeeeeeery hungry");
 
             if (collision.CompareTag("Note"))
             {
