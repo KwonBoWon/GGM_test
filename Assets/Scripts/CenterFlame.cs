c48a5fb08edd279b2159d464380936be765cfc27
PlugDev77 <ochh508@naver.com>
2022-07-06 12:47:03 +0900

    Debug Test

--- Assets/Scripts/CenterFlame.cs
+++ Assets/Scripts/CenterFlame.cs
@@ -16,6 +16,7 @@ public class CenterFlame : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(!musicStart){
+            Debug.Log("TEST");
             if (collision.CompareTag("Note"))
             {
                 myAudio.Play();
